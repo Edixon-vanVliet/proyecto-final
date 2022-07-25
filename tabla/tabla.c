b1d7b89545339cefc945918f00822fc59948695a
Jhon Gonzalez <jhon6051622@outlook.es>
2022-07-24 23:02:10 -0400

    Hacer trabajo de Jhon

--- tabla/tabla.c
+++ tabla/tabla.c
@@ -8,5 +8,40 @@
 
 void tabla()
 {
-    printf("Hello tabla\n");
+    int mayor = 0;
+    int menor = 0;
+    int numeros[100];
+    int cantidadNumeros = 0;
+    printf("Cuantos numeros desea introducir? ");
+    scanf("%d", &cantidadNumeros);
+    for (int i = 0; i < cantidadNumeros; i++)
+    {
+        printf("\nCual es valor numero #%d ", i + 1);
+        scanf("%d", &numeros[i]);
+        if (i == 0)
+        {
+
+            mayor = numeros[i];
+            menor = numeros[i];
+        }
+        else if (numeros[i] < menor)
+        {
+
+            menor = numeros[i];
+        }
+
+
+        else if (numeros[i] > mayor)
+        {
+            mayor = numeros[i];
+        }
+    }
+    printf("\nEl numero menor introducido es: %i ", menor);
+    printf("\nEl numero mayor introducido es: %i \n", mayor);
+
+    for (int i = 0; i < cantidadNumeros; i++)
+    {
+        printf("%d,", numeros[i]);
+    }
+    printf("\n");
 }
