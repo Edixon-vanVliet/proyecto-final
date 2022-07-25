d72442101a24c54fc90fe1270d1fe61fbf6d234e
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2022-07-25 00:58:52 -0400

    feat: Add sort array function to utils

--- utils/utils.c
+++ utils/utils.c
@@ -39,3 +39,19 @@ void print_character(char character, int times)
         printf("%c", character);
     }
 }
+
+void sort_array(float *array, int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = i + 1; j < size; ++j)
+        {
+            if (array[i] > array[j])
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
