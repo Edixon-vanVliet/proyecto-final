a6cf37e33bb7deac397b9f825a5188d72864735c
Bryant <bjimenezr8@gmail.com>
2022-07-24 23:17:20 -0400

    Codigo del programa de Algebra

--- algebra/algebra.c
+++ algebra/algebra.c
@@ -8,5 +8,45 @@
 
 void algebra()
 {
-    printf("Hello algebra\n");
+    int matriz[100];
+
+    int cantidad_numeros;
+    printf("Cuantos numeros quieres ordenar?\n");
+    scanf("%d", &cantidad_numeros);
+
+    for (int i = 0; i < cantidad_numeros; i++)
+    {
+        int x = 0;
+
+        printf("Inserte un numero:\n");
+        scanf("%d", &x);
+
+        if (x < 0)
+        {
+            matriz[i] = x * -1;
+        }
+        else
+        {
+            matriz[i] = x;
+        }
+    }
+
+    int i, element, j;
+    for (i = 1; i < cantidad_numeros; i++)
+    {
+        element = matriz[i];
+        j = i - 1;
+        while (j >= 0 && matriz[j] > element)
+        {
+            matriz[j + 1] = matriz[j];
+            j = j - 1;
+        }
+        matriz[j + 1] = element;
+    }
+
+    printf("\nLista ordenada:\n");
+    for (int i = 0; i < cantidad_numeros; i++)
+    {
+        printf("%d\n", matriz[i]);
+    }
 }
