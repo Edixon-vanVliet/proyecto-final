8097c7c8100ecc12ff08af1a292017cdfed4153c
Edixon van Vliet <40107868+Edixon-vanVliet@users.noreply.github.com>
2022-07-26 15:06:42 -0400

    Add cross-platform support

--- main.c
+++ main.c
@@ -47,7 +47,6 @@ int main()
             printf("Opcion elegida no disponible");
             sleep_console(2);
             clear_console();
-            fflush(stdin);
             break;
         }
     } while (option != 's');
