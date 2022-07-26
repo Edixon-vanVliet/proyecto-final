b119f60340801189f4d22ec4d335d5afb6b310f4
Bryant <bjimenezr8@gmail.com>
2022-07-25 22:21:25 -0400

    Refactorizar codigo

--- algebra/algebra.c
+++ algebra/algebra.c
@@ -1,52 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "algebra.h"
+#include "../utils/utils.h"
 
 // Hacer un programa que reordene una lista de números en una secuencia de
 // valores algebraicos en forma Ascendente (de menor a mayor) en valor absoluto,
 // y, de menor a mayor algebraicamente con signo, utilizando una
 // formación (matrices) unidimensional de enteros.
 
+int array_size;
+
+void get_array_size();
+void get_numbers(int *numbers, int *absolute_numbers);
+void sort(int *array);
+void print_numbers(char *message, int *array);
+
 void algebra()
 {
-    int matriz[100];
+    get_array_size();
 
-    int cantidad_numeros;
-    printf("Cuantos numeros quieres ordenar?\n");
-    scanf("%d", &cantidad_numeros);
+    int numbers[array_size], absolute_numbers[array_size];
 
-    for (int i = 0; i < cantidad_numeros; i++)
+    get_numbers(numbers, absolute_numbers);
+
+    sort(numbers);
+    sort(absolute_numbers);
+
+    print_numbers("Secuencia ordenada con signo: ", numbers);
+    print_numbers("Secuencia ordenada sin signo: ", absolute_numbers);
+
+    printf("\nPresione cualquier tecla para continuar.");
+    pause_console();
+}
+
+void get_array_size()
+{
+    printf("Cuantos numeros desea introducir?\n\n");
+    printf(">> ");
+    scanf("%d", &array_size);
+}
+
+void get_numbers(int *numbers, int *absolute_numbers)
+{
+    for (int i = 0; i < array_size; i++)
     {
-        int x = 0;
+        clear_console();
+        printf("Cual es valor numero #%d\n\n", i + 1);
+        printf(">> ");
+        scanf("%d", &numbers[i]);
 
-        printf("Inserte un numero:\n");
-        scanf("%d", &x);
+        absolute_numbers[i] = abs(numbers[i]);
+    }
 
-        if (x < 0)
+    clear_console();
+}
+
+void sort(int *array)
+{
+    for (int i = 0; i < array_size; ++i)
+    {
+        for (int j = i + 1; j < array_size; ++j)
         {
-            matriz[i] = x * -1;
+            if (array[i] > array[j])
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+void print_numbers(char *message, int *array)
+{
+    printf("%s", message);
+
+    for (int i = 0; i < array_size; i++)
+    {
+        printf("%d", array[i]);
+
+        if (i != array_size - 1)
+        {
+            printf(", ");
         }
         else
         {
-            matriz[i] = x;
+            printf("\n");
         }
-    }
-
-    int i, element, j;
-    for (i = 1; i < cantidad_numeros; i++)
-    {
-        element = matriz[i];
-        j = i - 1;
-        while (j >= 0 && matriz[j] > element)
-        {
-            matriz[j + 1] = matriz[j];
-            j = j - 1;
-        }
-        matriz[j + 1] = element;
-    }
-
-    printf("\nLista ordenada:\n");
-    for (int i = 0; i < cantidad_numeros; i++)
-    {
-        printf("%d\n", matriz[i]);
     }
 }
